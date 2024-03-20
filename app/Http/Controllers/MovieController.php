2945514cf4d73e4814b99fda4aff766b4d66f4af
lucapolzone <lucapolzone@gmail.com>
2024-03-20 12:35:15 +0100

    feat: print movie title

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    //Mostra la lista della risorsa Movie
+
+    public function index() {
+        $movies = Movie::all();
+        return view('movies.index', compact('movies'));
+    }
 }
---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('title', 'Lista dei film')
+
+@section('main-content')
+
+  <section>
+    <div class="container">
+      <h1>Movies</h1>
+      {{-- @dump($movies) --}}
+      <div class="row">
+
+        @forelse($movies as $movie)
+          <div class="col-4">
+            <div class="card">
+              <div class="card-body">
+                <h4>{{ $movie->title }}</h4>
+              </div>
+            </div>
+          </div>
+
+        @empty
+        <div class="col-12">
+          No books
+        </div>          
+        @endforelse          
+
+      </div>
+    </div>
+  </section>
+    
+@endsection
